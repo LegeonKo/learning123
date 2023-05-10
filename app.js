3532d7261edd1dd0318cb5cc31738c42fe504ec4
LegeoN <legeon0990@Yandex.ru>
2023-05-10 18:23:08 +0300

    timer msecs added

--- app.js
+++ app.js
@@ -5,32 +5,50 @@ const buttonStop = document.getElementById("button_stop");
 const timerCount = document.querySelector(".timer_count");
 const timerRecords = document.querySelector(".timer_records__text");
 
-let passedTime = 0;
-let timer;
-const newEvent = new CustomEvent("tick", {detail: {timer: `${passedTime}`}});
+let passedSecs = 0;
+let passedMsecs = 0;
+let Timer;
+const newEvent = new CustomEvent("tick", {
+    detail:
+        {
+            secs: `${passedSecs}`,
+            msecs: `${passedMsecs}`,
+        }
+});
 
 let buttonStopClicks = '0';
 
+buttonStop.disabled = true;
+
+
 buttonStart.addEventListener("tick", el => {
     timerCount.style.fontSize = '32px';
-    timerCount.innerText = `${el.detail.timer} с.`;
+    timerCount.innerText = `${el.detail.secs}.${el.detail.msecs} с.`;
 });
 
 buttonStart.addEventListener("click", () => {
-    timer = setInterval(()=> {
+    clearInterval(Timer);
+    buttonStop.disabled = false;
+    buttonStart.disabled = true;
+    Timer = setInterval(() => {
+        passedMsecs++;
         buttonStart.dispatchEvent(newEvent);
-        newEvent.detail.timer++;
-    },1000);
-
+        newEvent.detail.msecs = `${passedMsecs}`
+        if(passedMsecs === 100) {
+            passedMsecs = 0;
+            newEvent.detail.msecs = 0;
+            passedSecs++;
+            newEvent.detail.secs = `${passedSecs}`
+        }
+    }, 10);
 });
 
 buttonStop.addEventListener("click", () => {
-    if (newEvent.detail.timer === '0') {
-        return;
-    }
+    buttonStop.disabled = true;
+    buttonStart.disabled = false;
     buttonStopClicks++;
     let div = document.createElement("div");
     timerRecords.append(div);
     div.append(`${buttonStopClicks}. ${timerCount.innerText}`);
-    clearInterval(timer);
+    clearInterval(Timer);
 });
